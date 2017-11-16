2ecd4bb8259810414546627e184e53a01fc9cdf2
Inbar Pe'er <inbaronet@gmail.com>
2017-11-16 11:42:25 -0500

    lol

--- fork.c
+++ fork.c
@@ -9,13 +9,18 @@ int main(){
   int f=fork();
   if(f){
     fork();
-    int * status = wait();
+    wait();
+    printf("I am the parent and I'm done waiting. See ya later.");
   }
   if(!f){
-    sleep((rand() % (20+1-5))+5;);
-  }
- 
+    int mypid = getpid();
+    int sleepy = (rand() % (20+1-5))+5;
+    sleep(sleepy);
+    printf("I am a child. My pid number is %d and my nap took %d seconds.", mypid, sleepy);
     
+  }
+  return 0;
+}
     
   
   
