95af72ddbcf4c31086c55826f53f78f0975d17aa
Inbar Pe'er <inbaronet@gmail.com>
2017-11-16 21:25:50 -0500

    done

--- fork.c
+++ fork.c
@@ -2,22 +2,30 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 int main(){
+  int status;
+  srand(time(NULL));
   int PID = getpid();
-  printf("This is the initial statement\n");
-  int f=fork();
-  if(f){
-    fork();
-    wait(status);
-    printf("I am the parent and I'm done waiting. See ya later.");
+  printf("Hi I'm the parent, my pid number is %d,\n", PID);
+  pid_t f=fork();
+  int kid_pid;
+  if(f){ // if its the parent
+    kid_pid = wait(&status);
   }
   if(!f){
     int mypid = getpid();
-    int sleepy = (rand() % (20+1-5))+5;
+    int sleepy = rand()%16 + 5;
+    printf("I am child %d and I am going to sleep for %d seconds\n", mypid, sleepy);
     sleep(sleepy);
-    printf("I am a child. My pid number is %d and my nap took %d seconds.", mypid, sleepy);
-    
+    printf("I am child %d and I woke up from nap.\n", mypid);
+    exit(sleepy); 
+  }
+  else {
+    printf("I'm the parent and my child %d napped for %d secs \n", kid_pid, WEXITSTATUS(status));
+    printf("I'm the parent and I'm done with the program, bye!\n");
   }
   return 0;
 }
